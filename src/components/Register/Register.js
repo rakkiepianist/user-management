67a842db4f0f56de175a8bd74d9e48d4142b3447
Văn Linh <rakkiepianist@gmail.com>
2019-07-05 01:21:52 +0700

    Update register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Row, Col, Form, Icon, Input, Button, Typography } from 'antd';
 import './Register.css';
 
@@ -137,6 +138,7 @@ const Register = props => {
               >
                 Đăng ký
               </Button>
+              Đã có tài khoản? <Link to="/login">Đăng nhập!</Link>
             </Form.Item>
           </Form>
         </Col>
